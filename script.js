cdc2eaaecc3053bdf7c19159c53b4c69baccc164
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-13 10:48:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-const dateOfBirth = document.querySelector("#date-of-birth");
-const luckyNumber = document.querySelector("#lucky-number");
-const checkButton = document.querySelector("#calculate-number");
-const displayMessage = document.querySelector("#output-container");
+const date=document.querySelector("#date");
+const inputNumber=document.querySelector("#number");
+const outputTag = document.querySelector(".output");
+const reset = document.querySelector("#clear");
 
-const calculateSum = (date) => {
-  let sum = 0;
-  date = date.replaceAll("-", "");
-  for (let digit of date) {
-    sum = sum + Number(digit);
-  }
-  return sum;
-};
 
-const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
-  console.log(sumOfDate, numberToCheck);
-  if (sumOfDate % numberToCheck === 0) {
-    return showMessage(`${numberToCheck} is a lucky number!! 🥳 🥳 🥳 `);
-  }
-  showMessage(`${numberToCheck} is not that lucky 😕`);
-};
+const inputNumberValue = Number(inputNumber.value);
+const dateValue = date.value;
 
-const showMessage = (message) => {
-  displayMessage.innerText = message;
-};
+// const dobSum = (date) => {
 
-checkButton.addEventListener("click", () => {
-  const date = dateOfBirth.value;
-  const numberToCheck = luckyNumber.value;
-  if (date && numberToCheck) {
-    const sumOfDate = calculateSum(date);
-    checkIsNumberLucky(sumOfDate, numberToCheck);
-  } else {
-    showMessage("Please enter both the fields");
-  }
+//     let sum = 0;
+//     date = date.replaceAll("-", "");
+//     console.log(date);
+//     for (let digit of date) {
+//       sum = sum + Number(digit);
+//       console.log(digit);
+//     }
+//     return sum;
+// };
+
+// Calculating the luck
+function isLuckyNumber() {
+
+    let sum = 0;
+    // dateValue = dateValue.replaceAll("-", "");
+    console.log(inputNumberValue);
+    for (let digit of dateValue) {
+        if(!isNaN(digit)){
+            sum = sum + Number(digit);
+            console.log(digit);  
+        }
+        console.log(digit);
+    }
+    console.log(sum);
+    if(sum % inputNumberValue === 0) {
+        outputTag.innerHTML="Hurrah!, Your Birthday is Lucky."
+    }else {
+        outputTag.innerHTML="Sorry!, Your lucky number is not lucky enough to make your birthday lucky. Try with another lucky number.."
+    }
+}
+
+reset.addEventListener("click", function() {
+    outputTag.innerText = "";
+});
+
+document.getElementById("lucky-birth-form").addEventListener("submit", function(event) {
+    isLuckyNumber();
+    event.preventDefault();
 });
