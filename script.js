ce5969fe814b88c845d08622ffe190d7f3179f40
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-13 12:07:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,32 +3,75 @@ const inputNumber=document.querySelector("#number");
 const outputTag = document.querySelector(".output");
 const reset = document.querySelector("#clear");
 
+var inputNumberValue = Number("");
+inputNumber.addEventListener("input", function(){
+    inputNumberValue = Number(inputNumber.value);
+})
 
-const inputNumberValue = Number(inputNumber.value);
-const dateValue = date.value;
+var dateValue;
+date.addEventListener("input", function(){
+    dateValue = date.value;
+})
 
-// Calculating the luck
-function isLuckyNumber() {
-
+function dobSum(date){
     let sum = 0;
-    console.log(inputNumberValue);
-    for (let digit of dateValue) {
-        if(!isNaN(digit)){
+    for (let digit of date) {
+        if(!isNaN(Number(digit))){
             sum = sum + Number(digit);
             console.log(digit);  
         }
-        console.log(digit);
     }
     console.log(sum);
+    console.log(inputNumberValue);
+    return sum;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+// Calculating the luck
+async function isLuckyNumber() {
+
+    outputTag.innerText ="";
+    outputTag.style.textAlign = "left";
+    outputTag.setAttribute("placeholder", "Loading");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading.");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading..");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading...");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading..");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading.");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading.");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading..");
+    await sleep(500);
+    outputTag.setAttribute("placeholder", "Loading...");
+    await sleep(500);
+
+    var sum = dobSum(dateValue);
     if(sum % inputNumberValue === 0) {
-        outputTag.innerHTML="Hurrah!, Your Birthday is Lucky."
+        outputTag.innerHTML=`Hurrah!, Your Birthday "${dateValue}" is Lucky.`;
+        outputTag.style.textAlign = "center";
+        outputTag.style.color = "green";
     }else {
-        outputTag.innerHTML="Sorry!, Your lucky number is not lucky enough to make your birthday lucky. Try with another lucky number.."
+        outputTag.innerHTML=`Sorry!, Your lucky number "${inputNumberValue}" is not lucky enough to make your birthday "${dateValue}" lucky. Try with another lucky number..`;
+        outputTag.style.textAlign = "center";
+        outputTag.style.color = "red";
     }
 }
 
 reset.addEventListener("click", function() {
-    outputTag.innerText = "";
+  outputTag.innerText="";
+  outputTag.setAttribute("placeholder", "Output will be shown here...");
+  outputTag.style.textAlign = "left";
 });
 
 document.getElementById("lucky-birth-form").addEventListener("submit", function(event) {
